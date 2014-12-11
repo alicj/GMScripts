e97f4f51d0b69960780cbb42aa130fd05edd8708
AlicJ <zichen.jiang@yahoo.com>
2014-12-10 19:37:28 -0500

    add comments

--- Avenue_Pearson_E-text_Height_setter/Avenue_Pearson_E-text_Height_setter.user.js
+++ Avenue_Pearson_E-text_Height_setter/Avenue_Pearson_E-text_Height_setter.user.js
@@ -1,12 +1,18 @@
 // ==UserScript==
 // @name       Avenue Pearson E-text Height setter
 // @namespace  
-// @version    0.1
+// @version    1.0
 // @description  
 // @match      https://avenue.cllmcmaster.ca/d2l/le/content/*/fullscreen/*
 // @copyright  2014+, You
 
-// @grant      none 
+// @grant      none
+
+// This was for MyAccouringLab
+// Since the only way to access that is through avenue
+// and the pop up only shows half of the content on my 1080p screen
+// this script is used to show everything there
+// May not be super useful for everyone
 // ==/UserScript==
 $(document) .ready(function () {
   $('.d2l-popup-title').hide();
